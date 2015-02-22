a3cf8e0dc35bd831688fe7170813db0efdb8cc70
Amani Peddada <amanipeddada@rescomp-14-243751.stanford.edu>
2015-02-22 06:32:44 -0800

    Completed Tree class and basic testing

--- node.py
+++ node.py
@@ -7,63 +7,75 @@ from patch import Patch
 ##############################################################
 
 class Node:
-	def __init__(self, depth, patches=None, isLeaf=False):
-		self.depth = depth
-		self.isLeaf = isLeaf
-		self.children = None
-		self.binary_test = None
+  def __init__(self, patches=None, isLeaf=False, children=None, binary_test=None):
+    self.patches = patches
+    self.isLeaf = isLeaf
+    self.children = children
+    self.binary_test = binary_test
+    self.getParams()
 
-		if patches:
-			self.patches = patches
+  # Sets the children nodes of the node
+  def setChildren(self, children):
+    self.children = children
+    
+  # Sets the binary test associated with the node
+  def setBinaryTest(self, binary_test):
+    self.binary_test = binary_test
 
-	def setBinaryTest(self, binary_test):
-		self.binary_test = binary_test
+  # Computes the entropy for the node
+  def computeEntropy(self):
+    offset_cov, angle_cov = self.getCovariances()
+    print np.linalg.det(offset_cov)
+    print np.linalg.det(angle_cov)
+    entropy = np.log(np.linalg.det(offset_cov) + np.linalg.det(angle_cov))
+    print entropy
+    return entropy
 
-	# Computes the entropy for the node
-	def computeEntropy(self):
-		offset_cov, angle_cov = self.getCovariances()
-		entropy = np.log(np.linalg.det(offset_cov) + np.linalg.det(angle_cov))
-		return entropy
+  # Get covariance for theta offsets and theta angles
+  def getCovariances(self):
+    offset_cov = np.cov(self.theta_offsets)    # Covariance for theta offsets
+    angle_cov = np.cov(self.theta_angles)      # Covariance for theta angles
+    return offset_cov, angle_cov
 
-	# Get covariance for theta offsets and theta angles
-	def getCovariances(self):
-		offset_cov = np.cov(self.theta_offsets)    # Covariance for theta offsets
-		angle_cov = np.cov(self.theta_angles)      # Covariance for theta angles
-		return offset_cov, angle_cov
+  # Get mean for theta offsets and theta angles
+  def getMeans(self):
+   return np.mean(self.theta_offsets), np.mean(self.theta_angles)
 
-	# Get mean for theta offsets and theta angles
-	def getMeans(self):
-		 return np.mean(self.theta_offsets), np.mean(self.theta_angles)
+  # Get theta offsets and theta angles
+  def getParams(self):
+    theta_offsets = []
+    theta_angles = []
+    for patch in self.patches:
+      theta_offsets.append(patch.theta_offsets)
+      theta_angles.append(patch.theta_angles)
+    theta_offsets = np.concatenate(tuple(theta_offsets), axis=1)
+    theta_angles = np.concatenate(tuple(theta_angles), axis=1)
+    self.theta_offsets = theta_offsets
+    self.theta_angles = theta_angles
+    
+  # Returns the fraction of patches within the node that were taken from the head
+  def getPercentFromHead(self):
+    patchClasses = np.array([1 if patch.isFromHead else 0 for patch in self.patches])
+    return np.mean(patchClasses)       
 
-	# Get theta offsets and theta angles
-	def getParams(self):
-		theta_offsets = []
-		theta_angles = []
-		for patch in self.patches:
-			theta_offsets.append(patch.theta_offsets)
-			theta_angles.append(patch.theta_angles)
-		theta_offsets = np.concatenate(tuple(theta_offsets), axis=1)
-		theta_angles = np.concatenate(tuple(theta_angles), axis=1)
-		self.theta_offsets = theta_offsets
-		self.theta_angles = theta_angles
 
+  # Returns a list (size = 2) of candidate children based on the
+  # results of running the binary test
+  def getCandidateChildren(self, binary_test):
+    candidates = np.array([])
+    f1_corners, f2_corners, tau = binary_test['f1'], binary_test['f2'], binary_test['tau']
+    left_patches = np.array([])    # Patches assigned to the left child node
+    right_patches = np.array([])   # Patches assigned to the right child node
+    for patch in self.patches:
+      f1_data, f2_data = patch.getSubPatches(f1_corners, f2_corners)
+      meanDiff = patch.getSubPatchMeanDiff(f1_data, f2_data)
+      if meanDiff > tau:
+        right_patches = np.append(right_patches, patch)
+      else:
+        left_patches = np.append(left_patches, patch)
+    if len(left_patches) > 0 and len(right_patches > 0):
+      left_node = Node(patches=left_patches)
+      right_node = Node(patches=right_patches)
+      candidates = np.array([left_node, right_node])
 
-	# Returns a list (size = 2) of candidate children based on the
-	# results of running the binary test
-	def getCandidateChildren(self, binary_test):
-		f1_corners, f2_corners, tau = binary_test['f1'], binary_test['f2'], binary_test['tau']
-		left_patches = np.array([])    # Patches assigned to the left child node
-		right_patches = np.array([])   # Patches assigned to the right child node
-		for patch in self.patches:
-			f1_data, f2_data = patch.getSubPatches(f1_corners, f2_corners)
-			meanDiff = patch.getSubPatchMeanDiff(f1_data, f2_data)
-			if meanDiff > tau:
-				right_patches = np.append(right_patches, patch)
-			else
-				left_patches = np.append(left_patches, patch)
-
-		left_node = Node(patches=left_patches)
-		right_node = Node(patches=right_patches)
-		candidates = np.array([left_node, right_node])
-
-		return candidates
+    return candidates
--- tree.py
+++ tree.py
@@ -1,10 +1,120 @@
 import numpy as np
+import random
+from patch import Patch
+from node import Node
 
 ##############################################################
 # Class: Tree
 ##############################################################
 
 class Tree:
-	def __init__(self):
-		pass
-		
+  def __init__(self, patches, max_depth=10, min_patches=0, tau_range=[-10, 10], rate_change=10, root_node=None):
+    
+    #Stores all of the patches
+    self.patches = patches
+
+    #Stores the maximum allowed depth in the tree
+    self.max_depth = max_depth
+    
+    #Stores the minimum required patches per node   
+    self.min_patches = min_patches
+
+    #Stores the min and max allowed values of the threshold tau
+    self.tau_range = tau_range
+
+    self.rate_change = rate_change
+    
+    self.root_node = root_node
+    if root_node is None:
+      self.root_node = self.constructTree()
+
+  def constructTree(self):
+    current_depth = 0
+    root_node = Node(patches=self.patches, isLeaf=False)
+    root_node = self.constructTreeHelper(root_node, current_depth)
+    return root_node
+    
+  def constructTreeHelper(self, current_node, current_depth):
+    if current_depth == self.max_depth or len(current_node.patches) <= self.min_patches:
+      current_node.isLeaf = True
+      return current_node
+    best_binary_test = self.getBestBinaryTest(current_node, current_depth)
+    current_node.setBinaryTest(best_binary_test)
+    if not best_binary_test:
+      current_node.isLeaf = True
+      return current_node
+    candidate_children = current_node.getCandidateChildren(best_binary_test)
+    if len(candidate_children) == 0:
+      current_node.isLeaf = True
+      return current_node
+    left_child, right_child = candidate_children
+    left_child = self.constructTreeHelper(left_child, current_depth + 1)
+    right_child = self.constructTreeHelper(right_child, current_depth + 1)    
+    
+    current_node.setChildren(np.array([left_child, right_child]))
+    
+    return current_node
+  
+  def getBestBinaryTest(self, current_node, current_depth):
+    best_binary_test = None
+    best_info_gain = float('-inf')
+    for test in range(10000):
+      current_test = self.generateBinaryTest()
+      candidate_children = current_node.getCandidateChildren(current_test)
+      if len(candidate_children) == 0:
+        continue
+      current_info_gain = self.computeInfoGain(current_node, candidate_children[0], candidate_children[1], current_depth)
+      print current_info_gain
+      if current_info_gain >= best_info_gain:
+        best_info_gain = current_info_gain
+        best_binary_test = current_test
+    return best_binary_test
+
+
+  def generateBinaryTest(self):
+    f1_corners = self.computeRandomF()
+    f2_corners = self.computeRandomF()
+    tau = random.uniform(self.tau_range[0], self.tau_range[1])
+    binary_test = {'f1': f1_corners, 'f2': f2_corners, 'tau': tau}
+    return binary_test
+
+  
+  def computeRandomF(self):
+    max_x_value = self.patches[0].width
+    max_y_value = self.patches[0].height
+    upper_corner = [np.random.randint(0, max_x_value), np.random.randint(0, max_y_value)]
+    bottom_corner = [np.random.randint(upper_corner[0], max_x_value), \
+                     np.random.randint(upper_corner[1], max_y_value)]
+    return np.array([upper_corner, bottom_corner])
+      
+  
+  def computeInfoGain(self, current_node, left_child, right_child, depth):
+    UC = self.computeUC(left_child, right_child)
+    UR = self.computeUR(current_node, left_child, right_child)
+    return (UC + (1 - np.exp(-depth/self.rate_change)*UR))
+
+
+  
+  def computeUC(self, left_child, right_child):
+    PL_size = len(left_child.patches)
+    PR_size = len(right_child.patches)
+    left_percent_from_head = left_child.getPercentFromHead()
+    right_percent_from_head = right_child.getPercentFromHead()
+
+    numer = PL_size*(left_percent_from_head*np.log(left_percent_from_head) + (1 - left_percent_from_head)*np.log(1 - left_percent_from_head))+ \
+            PL_size*(right_percent_from_head*np.log(right_percent_from_head) + (1 - right_percent_from_head)*np.log(1 - right_percent_from_head))
+    denom = PL_size + PR_size
+
+    return (numer/denom)
+
+  def computeUR(self, current_node, left_child, right_child):
+    self_entropy = current_node.computeEntropy()
+    left_entropy = left_child.computeEntropy()
+    right_entropy = right_child.computeEntropy()
+
+    w_l = len(left_child.patches)/len(current_node.patches)
+    w_r = len(right_child.patches)/len(current_node.patches)
+
+    info_gain = self_entropy - (w_l*left_entropy + w_r*right_entropy)
+    return info_gain
+     
