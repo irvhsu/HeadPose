e04474c78cba4ba6c9935e8aed4a5b783d2f3c60
Irving Hsu <irvhsu@stanford.edu>
2015-03-17 06:49:20 -0700

    Modify build tree function to be generalizable to a number of folders

--- build_tree.py
+++ build_tree.py
@@ -1,25 +1,28 @@
 import numpy as np
 from tree import Tree
 
-def buildTree(folderNumber, treeID, numTrees=10):
+def buildTree(folderNumbers, treeID, numTrees=10):
+  all_patches = []
+  for folderNumber in folderNumbers: 
+  # Filename to read patches from
+    filename = "patches_" + str(folderNumber) + ".npy"
+    curr_patches = np.load(filename)
+    all_patches.append(curr_patches)
+    
+  all_patches = np.concatenate(all_patches)
+  
+  # Determine the number of patches to train the tree on
+  num_patches = len(all_patches)
+  num_train_patches = np.ceil(num_patches/numTrees)
+  
+  # Get random subset of all patches
+  random_indices = np.random.choice(range(num_patches), replace=False, size=num_train_patches)
+  train_patches = all_patches[random_indices]
+  
+  # Train tree on this random subset
+  curr_tree = Tree(patches=train_patches)
+  
+  # Write tree to a file
+  outFile = "tree_combined_" + str(treeID) + ".npy"
+  np.save(outFile, curr_tree)
 
-	# Filename to read patches from
-	filename = "patches_" + str(folderNumber) + ".npy"
-
-	# Get the actual patches
-	all_patches = np.load(filename)
-
-	# Determine the number of patches to train the tree on
-	num_patches = len(all_patches)
-	num_train_patches = np.ceil(num_patches/numTrees)
-
-	# Get random subset of all patches
-	random_indices = np.random.choice(range(num_patches), replace=False, size=num_train_patches)
-	train_patches = all_patches[random_indices]
-
-	# Train tree on this random subset
-	curr_tree = Tree(patches=train_patches)
-
-	# Write tree to a file
-	outFile = "tree" + str(treeID) + ".npy"
-	np.save(outFile, curr_tree)
